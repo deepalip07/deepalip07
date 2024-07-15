2c8f5b9aaf54f987b00d6e49af79cce4925f2f70
deepalip07 <deepalip071992@gmail.com>
2024-07-15 23:41:26 +0530

    Exception Handling Practice Prog

---
+++ JavaPracticeProjects/Array_default_values.java
@@ -0,0 +1,22 @@
+package JavaPracticeProjects;
+
+import java.util.Arrays;
+
+public class Array_default_values 
+{
+	public static void main(String[] args) 
+	{
+		int a[]=new int[3];
+		a[0]=10;
+		a[1]=20;
+		System.out.println(Arrays.toString(a));
+		
+		String b[]=new String[3];
+		b[0]="Deepali";
+		b[1]="abc";
+		System.out.println(Arrays.toString(b));
+		
+		
+	}
+
+}
