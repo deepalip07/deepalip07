5a8b701e8a8bbdd53ca35dcb658b6b14f62a1221
deepalip07 <deepalip071992@gmail.com>
2024-07-16 00:25:07 +0530

    Common element in 2 arrays prog

--- JavaPracticeProjects/Find_common_element_in_2arrays.java
+++ JavaPracticeProjects/Find_common_element_in_2arrays.java
@@ -1,12 +1,30 @@
 package JavaPracticeProjects;
 
+import java.util.Arrays;
+
 public class Find_common_element_in_2arrays 
 {
-	 int j11;
+	 
 	public static void main(String[] args) 
 	{
+		int a[]= {1,2,3,4,5,7,8};
+		int b[]= {2,3,0,11,12,7,8};
+		
+		System.out.println("Array1-->"+Arrays.toString(a));
+		System.out.println("Array2-->"+Arrays.toString(b));
+		for(int i=0;i<a.length;i++)
+		{
+			for(int j=0;j<b.length;j++)
+			{
+				if(a[i]==b[j])
+				{
+					System.out.println("Common element in given array-->"+a[i]);
+					
+				}
+			}
+		}
 	 
-	  }
+	 }
 	 
 	  
 
