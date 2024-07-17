bf89dbbb92f6cb5d89973f3df63ab3e194d0fe7f
deepalip07 <deepalip071992@gmail.com>
2024-07-17 06:38:03 +0530

    Tree set prog1

---
+++ JavaPracticeProjects/TreeSet_prog.java
@@ -0,0 +1,23 @@
+package JavaPracticeProjects;
+import java.util.*;  
+public class TreeSet_prog 
+{
+
+	public static void main(String[] args) 
+	{
+	   TreeSet<Integer> t=new TreeSet<> ();
+	   
+	   t.add(10);
+	   t.add(90);
+	   t.add(44);
+	   t.add(10);
+	  // t.add(null);
+	//   t.add("deepali");
+	   
+	   //Collections.sort(t);
+	   
+	   System.out.println(t);
+
+	}
+
+}
