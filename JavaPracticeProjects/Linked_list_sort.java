43382010d5655c951a3a9fe507d7cf28c08f97e4
deepalip07 <deepalip071992@gmail.com>
2024-07-17 06:28:55 +0530

    Linked list prog

---
+++ JavaPracticeProjects/Linked_list_sort.java
@@ -0,0 +1,25 @@
+package JavaPracticeProjects;
+
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class Linked_list_sort 
+{
+
+	public static void main(String[] args) 
+	{
+	  LinkedList l1=new LinkedList();
+	  l1.add(10);
+	  l1.add(90);
+	  l1.add(12);
+	  l1.add(27);
+	  l1.add(1);
+	  l1.add(null);
+	  l1.add(90);
+	  
+	  //Collections.sort(l1);
+	  System.out.println(l1);
+   
+	}
+
+}
