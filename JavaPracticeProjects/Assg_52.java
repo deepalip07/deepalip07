5c8c00297ad1b6f0831252a46cdda2b7d5a97826
deepalip07 <deepalip071992@gmail.com>
2024-07-21 19:59:17 +0530

    Assg 52

---
+++ JavaPracticeProjects/Assg_52.java
@@ -0,0 +1,14 @@
+package JavaPracticeProjects;
+
+public class Assg_52 {
+
+	public static void main(String[] args) 
+	{
+		byte a;
+		int b=122398888;
+		 a= (byte) b;
+		 System.out.println(a);
+	
+	}
+
+}
