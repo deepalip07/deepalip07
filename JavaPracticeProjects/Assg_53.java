59cc8ed949686f1980608c9a61eb817211d2ed8b
deepalip07 <deepalip071992@gmail.com>
2024-07-21 23:12:59 +0530

    Assg 53

---
+++ JavaPracticeProjects/Assg_53.java
@@ -0,0 +1,15 @@
+package JavaPracticeProjects;
+
+public class Assg_53 {
+
+	public static void main(String[] args) 
+	{
+		double d=129999.222;
+		float a=12333.11f;
+		
+		a=(float) d;
+		System.out.println(a);
+		
+	}
+
+}
