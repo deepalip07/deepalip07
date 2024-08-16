cfedea6cea733c76350ca8bffbeae88464127c11
deepalip07 <deepalip071992@gmail.com>
2024-08-16 13:58:22 +0530

    SIB IIB Concept

---
+++ JavaPracticeProjects/SIB_IIB_concept.java
@@ -0,0 +1,32 @@
+package JavaPracticeProjects;
+
+public class SIB_IIB_concept 
+{
+	static
+	{
+		System.out.println("SIB");
+	}
+	
+	SIB_IIB_concept()
+	{
+		System.out.println("Constructor");
+	}
+	
+	{
+		System.out.println("IIB_01");
+	}
+	{
+		System.out.println("IIB_02");
+	}
+	
+	
+
+	public static void main(String[] args) 
+	{
+		SIB_IIB_concept s=new SIB_IIB_concept();
+		SIB_IIB_concept s1=new SIB_IIB_concept();
+		//SIB_IIB_concept s2=new SIB_IIB_concept();
+		
+	}
+
+}
