1e0f382076c02c6a06d0a21749ef54b4c575b768
deepalip07 <deepalip071992@gmail.com>
2024-08-17 15:52:24 +0530

    Find Current time and date

---
+++ JavaPracticeProjects/Find_current_time.java
@@ -0,0 +1,21 @@
+package Assignments;
+
+import java.util.Date;
+
+public class Find_current_time 
+{
+
+	public static void main(String[] args) 
+	{
+	     Date d=new Date();
+	     System.out.println(d.getTime());
+	     
+	     Date d1=new Date();
+	     System.out.println("Current Time--->"+d1);
+	     
+	
+	     
+	}
+
+}
+
