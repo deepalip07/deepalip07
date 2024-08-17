a3eb5b3f5c562dc8ee6323167747d925cf412c29
deepalip07 <deepalip071992@gmail.com>
2024-08-17 16:38:57 +0530

    continue keyword

---
+++ JavaPracticeProjects/continue_keyword.java
@@ -0,0 +1,22 @@
+package Assignments;
+
+public class continue_keyword {
+
+	public static void main(String[] args) 
+	{
+		int i = 0;
+		for( i=0;i<=5;i++)
+		{
+			if(i==4)
+			{
+			continue;
+			}
+			
+			System.out.println(i);
+
+		}
+		
+	
+	}
+
+}
