3508812059b4ed4630814e63738e7f2bbd8e9ff6
deepalip07 <deepalip071992@gmail.com>
2024-08-19 01:40:18 +0530

    widening type casting

---
+++ JavaPracticeProjects/Widening_type_casting.java
@@ -0,0 +1,17 @@
+package Assignments;
+
+public class Widening_type_casting {
+
+	public static void main(String[] args) 
+	{
+	   int i=10;
+	   long b=i;
+	   double c=i;
+	   System.out.println("Integer-->"+i);
+	   System.out.println("Long-->"+b);
+	   System.out.println("Double-->"+c);
+
+
+	}
+
+}
