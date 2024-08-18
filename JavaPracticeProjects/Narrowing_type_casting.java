611a3c9685839a322852386a72fddba633f9d288
deepalip07 <deepalip071992@gmail.com>
2024-08-19 01:43:16 +0530

    narrowing type casting

---
+++ JavaPracticeProjects/Narrowing_type_casting.java
@@ -0,0 +1,15 @@
+package Assignments;
+
+public class Narrowing_type_casting {
+
+	public static void main(String[] args) 
+	{
+		double d=90.222;
+		int i=(int) d;
+		
+		System.out.println(d);
+		System.out.println(i);
+
+	}
+
+}
