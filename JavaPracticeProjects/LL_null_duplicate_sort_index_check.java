1ce84568c9573a2f896adc1b1f6aa037ad0b79b6
deepalip07 <deepalip071992@gmail.com>
2024-08-19 01:06:13 +0530

    LL null duplicate indexing check

---
+++ JavaPracticeProjects/LL_null_duplicate_sort_index_check.java
@@ -0,0 +1,26 @@
+package Assignments;
+
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class LL_null_duplicate_sort_index_check {
+
+	public static void main(String[] args) 
+	{
+		 LinkedList l=new LinkedList();
+			
+			l.add(10);
+			l.add(100);
+			l.add(21);
+			l.add(32);
+			l.add(null);
+		l.add(100);
+			
+			System.out.println("Original LinkedList-->"+l);
+			
+	//Collections.sort(l);
+	//System.out.println("Sorted linked list--->"+l);
+		
+	}
+
+}
