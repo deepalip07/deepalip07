28c8094676ac5a63949e8dcee5ab710bf05742a7
deepalip07 <deepalip071992@gmail.com>
2024-08-19 03:08:55 +0530

    for each loop

---
+++ JavaPracticeProjects/For_each_loop.java
@@ -0,0 +1,15 @@
+package Assignments;
+
+public class For_each_loop {
+
+	public static void main(String[] args) 
+	{
+	 
+		int ar[] = { 10, 50, 60, 80, 90 }; 
+		  
+        for (int element : ar) 
+  
+            System.out.print(element + " "); 
+	}
+
+}
