d620838f0db73751c19e105874f33fd41b69ba4d
deepalip07 <deepalip071992@gmail.com>
2024-08-19 02:45:24 +0530

    While loop

---
+++ JavaPracticeProjects/While_loop.java
@@ -0,0 +1,17 @@
+package JavaPracticeProjects;
+
+public class While_loop {
+
+	public static void main(String[] args) 
+	
+	{
+		
+        int i = 1;
+        while (i < 6) 
+        {
+            System.out.println("While loop executed...!");
+            i++;
+        }
+	}
+
+}
