c56b636c8db587772e531b0ca1aee95cf145efd2
deepalip07 <deepalip071992@gmail.com>
2024-08-18 22:05:34 +0530

    If statement

---
+++ JavaPracticeProjects/If_stmt.java
@@ -0,0 +1,18 @@
+package Assignments;
+
+public class If_stmt {
+
+	public static void main(String[] args) 
+	{
+
+		int age=18;
+		
+		if(age>=18)
+		{
+			System.out.println("Eligible for voting");
+		}
+		
+
+	}
+
+}
